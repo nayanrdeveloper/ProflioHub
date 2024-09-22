30df2cbf9ba442a1fea5b8f4934205004361117f
nayanrdeveloper <nayanrdeveloper@gmail.com>
2024-09-22 15:23:04 +0530

    setup of eslint, prettier all style guide maintian

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,16 @@
-import LoginScreen from "@/src/screen/Login/LoginScreen";
-import { Text, View } from "react-native";
+import LoginScreen from '@/src/screen/Login/LoginScreen';
+import { Text, View } from 'react-native';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        // alignItems: "center",
-      }}
-    >
-     <LoginScreen />
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                // alignItems: "center",
+            }}
+        >
+            <LoginScreen />
+        </View>
+    );
 }
--- src/screen/Login/LoginScreen.tsx
+++ src/screen/Login/LoginScreen.tsx
@@ -1,35 +1,47 @@
-import { View, Text, Image, StyleSheet, TextInput } from 'react-native'
-import React from 'react'
-import Input from '@/src/components/common/Input'
-import CommonLayout from '@/src/components/layouts/CommonLayouts'
-import CommonButton from '@/src/components/common/CommonButton'
+import { View, Text, Image, StyleSheet, TextInput } from 'react-native';
+import React from 'react';
+import Input from '@/src/components/common/Input';
+import CommonLayout from '@/src/components/layouts/CommonLayouts';
+import CommonButton from '@/src/components/common/CommonButton';
 
 export default function LoginScreen() {
-  const onchangeEmail = () => {
-    console.log("Hello")
-  }
+    const onchangeEmail = () => {
+        console.log('Hello');
+    };
 
-  const onHandleLogin = () => {
+    const onHandleLogin = () => {};
 
-  }
-
-  return (
-    <CommonLayout>
-        <View>
-          <Image source={require('../../../assets/images/logo/logo_favicon.png')} style={styles.logo_image} />
-        </View>
-        <View>
-          <Input label='Email' placeholder='Enter your email' onChangeText={onchangeEmail} value='nayanrdeveloper@gmail.com' />
-          <Input label='Password' secureTextEntry placeholder='Enter your email' onChangeText={onchangeEmail} value='nayanrdeveloper@gmail.com' />
-        </View>
-        <CommonButton title='Submit' onPress={onHandleLogin} />
+    return (
+        <CommonLayout>
+            <View>
+                <Image
+                    source={require('../../../assets/images/logo/logo_favicon.png')}
+                    style={styles.logo_image}
+                />
+            </View>
+            <View>
+                <Input
+                    label="Email"
+                    placeholder="Enter your email"
+                    onChangeText={onchangeEmail}
+                    value="nayanrdeveloper@gmail.com"
+                />
+                <Input
+                    label="Password"
+                    secureTextEntry
+                    placeholder="Enter your email"
+                    onChangeText={onchangeEmail}
+                    value="nayanrdeveloper@gmail.com"
+                />
+            </View>
+            <CommonButton title="Submit" onPress={onHandleLogin} />
         </CommonLayout>
-  )
+    );
 }
 
 const styles = StyleSheet.create({
-  logo_image: {
-    width: 100,
-    height: 100,
-  },
-})
+    logo_image: {
+        width: 100,
+        height: 100,
+    },
+});
